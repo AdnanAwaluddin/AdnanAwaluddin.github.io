e1e167250655415aa92a30d1df865b591ca4e9be
Adnan Awaluddin <adnanawaluddin8115@gmail.com>
2024-12-03 23:55:20 +0700

    update pt 3

--- Js/indeks.js
+++ Js/indeks.js
@@ -3,6 +3,16 @@ function hitung(){
     let tinggi = document.getElementById("tinggi-badan-input").value;
     let usia = document.getElementById("usia-input").value;
     
-    let hasil = parseInt(berat)/(parseInt(tinggi)^2);
-    document.writeln("Hasilnya : " + hasil);
+    let hasil = (parseInt(berat))/(parseFloat(tinggi)**2)*10000;
+    document.writeln("Hasilnya : " + hasil.toFixed(1));
+    let kategori;
+    if (hasil < 18.5) {
+        kategori = "Anda mengalami kekurangan berat badan";
+    } else if (hasil < 24.9){
+        kategori = "Anda memiliki berat badan normal(ideal)";
+    } else if (hasil < 29.9){
+        kategori = "Anda memiliki berat badan berlebihan";
+    } else {kategori = "Anda mengalami kegemukan (obesitas)"
+    }
+    document.writeln("Kategori : " + kategori)
 }
